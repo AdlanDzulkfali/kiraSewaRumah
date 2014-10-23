d205c31abcfb3e493d10befe84f180222192530a
Adstyle <mohammadadlan91@gmail.com>
2014-10-23 23:55:48 +0800

    kiraRumahSewa App

--- kiraSewaRumah/ViewController.swift
+++ kiraSewaRumah/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var houseRentText: UITextField!
+    @IBOutlet weak var electricBillText: UITextField!
+    @IBOutlet weak var waterBillText: UITextField!
+    @IBOutlet weak var internetBillText: UITextField!
+    @IBOutlet weak var totalMatesText: UITextField!
+    
+    @IBOutlet weak var rentCalcLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculateAll(sender: UIButton) {
+        let homeRent = Double((houseRentText.text as NSString).doubleValue)
+        let waterBil = Double((waterBillText.text as NSString).doubleValue)
+        let internetBil = Double((internetBillText.text as NSString).doubleValue)
+        let electricBil = Double((electricBillText.text as NSString).doubleValue)
+        let totalMates = Double((totalMatesText.text as NSString).doubleValue)
+        
+        rentCalcLabel.hidden = false
+        totalMatesText.resignFirstResponder()
+        rentCalcLabel.text = "\((homeRent + waterBil + internetBil + electricBil) / totalMates)"
+    }
 
 }
 
